733206c3c90ca6b4f16f9f8ababb8910962b221e
AFSAL P U <afsalpu16@gmail.com>
2023-05-24 19:33:44 +0530

    almost finished

--- admin/src/components/navbar/Navbar.jsx
+++ admin/src/components/navbar/Navbar.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
 
 import './Navbar.scss'
+import {AuthContext} from '../../context/AuthContext'
 
 const Navbar = () => {
+  const {admin, setAdmin} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
         <Link to="/">All Orders</Link>
         <Link to="/all-doctors">All Doctors</Link>
         <Link to="/all-services">All Services</Link>
-        <Link to="/">Logout</Link>
+        {admin && (
+          <button onClick={() => setAdmin(null)}>Logout</button>
+        )}
     </div>
   )
 }
